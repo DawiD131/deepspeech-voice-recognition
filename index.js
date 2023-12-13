673531f474a5a839fc1baca5c1cb75a3560c10c9
dawidkutwin <dawid.kutwin200@gmail.com>
2023-12-13 18:37:47 +0100

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const DeepSpeech = require('deepspeech')
+const fs = require('fs');
+
+require('dotenv').config();
+
+
+const MODEL_PATH = process.env.MODEL_PATH;
+const SCORER_PATH = process.env.SCORER_PATH;
+
+const AUDIO_PATH = './audio.wav';
+
+const KEYWORD = 'hello';
+
+const model = new DeepSpeech.Model(MODEL_PATH);
+model.enableExternalScorer(SCORER_PATH);
+
+const audioBuffer = fs.readFileSync(AUDIO_PATH);
+const audioData = new Int16Array(audioBuffer.buffer);
+
+const result = model.stt(audioData);
+
+if (result.toLowerCase().includes(KEYWORD.toLowerCase())) {
+    console.log(`Keyword "${KEYWORD}"`);
+} else {
+    console.log(`not found`);
+}
